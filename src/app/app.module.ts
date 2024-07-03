d7a120997d7ef6e38a481521676fc851327d28fb
Tobias Schmidt <tobias.schmidt@dedalus.eu>
2024-07-03 14:23:24 +0200

    routing works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,28 @@ import { AbstractAirportService } from './airports/abstract-airport.service';
 import { AirportService } from './airports/airport.service';
 import { SharedModule } from '../shared/shared.module';
 import { FlightBookingModule } from '../flight-booking/flight-booking.module';
+import { HomeComponent } from './home/home.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+import { RouterModule } from '@angular/router';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FormsModule, SharedModule, FlightBookingModule],
-  declarations: [AppComponent, NavbarComponent, SidebarComponent, AirportsComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    SharedModule,
+    FlightBookingModule,
+    RouterModule.forRoot(APP_ROUTES)
+  ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    AirportsComponent,
+    HomeComponent,
+    PassengerSearchComponent
+  ],
   bootstrap: [AppComponent],
   providers: [
     {
